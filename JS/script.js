08aed07082590fd9e1b50b7f14082440f36a6dd9
Lucas Macedo <lucas.macedo@gsuite.iff.edu.br>
2022-08-22 15:07:03 -0300

    versao final

--- JS/script.js
+++ JS/script.js
@@ -6,11 +6,12 @@ let total_certas=0;
 let jogadas=0;
 let id=0;
 let tempo;
+
 function quantidade_cartas(){
     parrots.sort(comparador);
-    cartas=prompt('Digite a quantidade de cartas');
+    cartas=prompt('Digite a quantidade de cartas para joga (entre 4 e 14, e par)');
     while (cartas%2!==0||cartas<4||cartas>14){
-        cartas=prompt('Digite a quantidade de cartas correta entre 4 e 14');
+        cartas=prompt('Digite a quantidade de cartas correta (entre 4 e 14, e par)');
     }
     embaralhar();
     let b=``
@@ -26,10 +27,12 @@ function quantidade_cartas(){
     f.innerHTML=b;
     coluna_cartas();
 }
+
 window.addEventListener('resize', function () {
     //var altura = window.innerHeight;
     coluna_cartas();
 });
+
 function coluna_cartas(){
     for (i=1;i<=7;i++){
         document.querySelector('.tabulheiro').classList.remove(`t${i}`);}
@@ -53,11 +56,29 @@ function coluna_cartas(){
         a=5;}
     document.querySelector('.tabulheiro').classList.add(`t${a}`);
 }
-/*var viewport_width = window.innerWidth;*/
+
+function luz(){
+    document.querySelector('.temporizador').classList.toggle('luz');
+}
+
+function letra(){
+    document.querySelector('.temporizador p').classList.toggle('letra');
+}
+
+function animacao(){
+    luz();
+    letra();
+}
 
 function virar(caminho){
     if(jogadas===0){
         temporizador();
+        luz();
+        letra();
+        setTimeout(animacao,150);
+        setTimeout(animacao,300);
+        setTimeout(animacao,450);
+        
     }
     if(caminho!==dupla_virada[0]){
         if (dupla_virada.length<2&&!(caminho.classList.contains('acertou'))){
@@ -76,6 +97,7 @@ function virar(caminho){
         }
     }
 }
+
 function acertou(){
     if (dupla_virada.length<2){
         return 0;
@@ -99,10 +121,12 @@ function girar_2_cartas(){
     girar(dupla_virada[1]);
     dupla_virada=[];
 }
+
 function girar(caminho){
     caminho.querySelector('.frente').classList.toggle('girar-frente');
     caminho.querySelector('.tras').classList.toggle('girar-tras');
 }
+
 function embaralhar(){
     lista=[];
     for(i=0;i<cartas/2;i++){
@@ -120,20 +144,28 @@ function temporizador(){
     document.querySelector('.temporizador p').innerHTML='0 s';
     id=setInterval(relogio,1000);
 }
+
 function relogio(){
     let a=document.querySelector('.temporizador p');
-
     let b=Number(a.innerHTML.replace(/[^0-9]/g,''))+1;
     b=b+' s';
     a.innerHTML=b;
 }
+
 function fim_de_jogo(){
     alert(`Você ganhou em ${jogadas} jogadas e com ${tempo} segundos.`);
     let jogar=prompt('Deseja jogar novamente sim ou não?');
+    while(jogar!='sim'&&jogar!='não'){
+        jogar=prompt('Deseja jogar novamente sim ou não?');
+    }
     if (jogar==='sim'){
         comecar_jogo();
     }
+    else{
+        alert('Tchal');
+    }
 }
+
 function comecar_jogo(){
     document.querySelector('.temporizador p').innerHTML='0 s';
     dupla_virada=[];
@@ -142,4 +174,5 @@ function comecar_jogo(){
     jogadas=0;
     quantidade_cartas();
 }
+
 comecar_jogo();
